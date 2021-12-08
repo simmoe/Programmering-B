6a0eac9488c64bbc75fc41aae9669ed338209edb
Simon <simmoe@gmail.com>
2021-12-08 15:47:50 +0100

    Added textelement

--- 15-KALENDER-EKSPERIMENT/index.js
+++ 15-KALENDER-EKSPERIMENT/index.js
@@ -13,9 +13,18 @@ fetch('./kalender.json')
             let lock = document.createElement('div')
             lock.classList.add('lock')
             lock.innerHTML = door.date
-            
-            
-            
+
+            //tilføj animation når der klikkes på lågen
+            lock.addEventListener('click', ()=>{
+                lock.innerHTML = ''
+                switch(door.animation){
+                    case 'slide':
+                        lock.classList.add('openSlide')
+                        break;
+                }
+            })
+
+            //sæt billede og andet indhold ind jvf JSON dokumentet
             switch(door.content){
                 case 'image': 
                 setImage(div, door)
@@ -27,7 +36,13 @@ fetch('./kalender.json')
                         break
                     }
                     
-                    
+            //læg tekst på lågen hvis der står noget i JSON
+            if(door.text){
+                let textElement = document.createElement('p')
+                textElement.innerHTML = door.text
+                div.append(textElement)
+            } 
+
             div.append(lock)
             document.querySelector('main').append(div)
         })
